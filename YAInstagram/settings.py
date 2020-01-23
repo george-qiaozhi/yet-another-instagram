151c4a8f7cdf4c392c40234b46a90654d5dbe2a0
george-qiaozhi <george.qiaozhi@gmail.com>
2020-01-23 14:08:54 -0600

    urlConfig and add basic view for static page

--- YAInstagram/settings.py
+++ YAInstagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%ym!7jxsv2h9)lm^=_lp^5cc_jof-x%*d!b$*#*0p+sf3bx*l)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instaAPP',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'YAInstagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
