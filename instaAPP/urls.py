db8127fe23c8dd1742f1e5fce888ee1f5c6d012c
george-qiaozhi <george.qiaozhi@gmail.com>
2020-01-24 14:28:06 -0600

    add user CRUD functions and the corresponding template

--- instaAPP/urls.py
+++ instaAPP/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import HelloWorld
+from .views import HelloWorld, PostsListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='home'),
+    path('posts/', PostsListView.as_view(), name='posts'),
+    #<> provide extra parameter, int type, pk primary key
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'), 
+    path('post/edit/<int:pk>', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
 ]
--- instaAPP/views.py
+++ instaAPP/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 class HelloWorld(TemplateView):
-    template_name = 'test.html'
+    template_name = "home.html"
+
+class PostsListView(ListView):
+    model = Post
+    template_name = "master_view.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_update.html"
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    # cannot delete & reverse at the same time, so delay reverse
+    success_url = reverse_lazy("posts")
